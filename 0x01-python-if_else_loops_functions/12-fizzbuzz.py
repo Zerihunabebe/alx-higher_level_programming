bd8a6b2064f6865715b58a0376f003b75347db88
Zerihunabebe <abebezerihunalx@gmail.com>
2022-05-31 15:07:13 -0700

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
-def pow(a, b):
-    """Return a to the power of b"""
-    return (a ** b)
+def fizzbuzz():
+    """Print the numbers from 1 to 100 separated by a space.
+
+    For multiples of three, print Fizz instead of the number.
+    For multiples of five, print Buzz instead of the number.
+    for multiples of three and five, print FizzBuzz instead of the number.
+    """
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif number % 3 == 0:
+            print("Fizz ", end="")
+        elif number % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(number), end="")
