a03f351f38f72b2b922953685f463a01b17f73d1
Zerihunabebe <abebezerihunalx.com>
2022-06-05 01:47:48 -0700

    9-max-integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list_ == 0:
-            return (None)
+    if len(my_list) == 0:
+        return (None)
 
     big = my_list[0]
     for i in range(len(my_list)):
