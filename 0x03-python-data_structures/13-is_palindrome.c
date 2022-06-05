9516ca87d95d5e71444f4b0b75761695fcb305fe
Zerihunabebe <abebezerihunalx.com>
2022-06-05 04:40:22 -0700

    reverses second half of the list

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,29 +1,29 @@
-#include "lists.h"
+#include "list.h"
 
 /**
  * reverse - reverses the second half of the list
  *
  * @h_r: head of the second half
- * Return: no return
+ * return: no return
  */
 void reverse(listint_t **h_r)
 {
-    listint_t *prv;
-    listint_t *crr;
-    listint_t *nxt;
-    
-    prv = NULL;
-    crr = *h_r;
-    
-    while (crr != NULL)
-    {
-	nxt = crr->next;
-	crr->next = prv;
-	prv = crr;
-	crr = nxt;
-    }
-    
-    *h_r = prv;
+	listint_t *prv;
+	listint_t *crr;
+	listint_t *nxt;
+
+	prv = NULL;
+	crr = *h_r;
+
+	while (crr != NULL)
+	{
+		nxt = crr->next;
+		crr->next = prv;
+		prv = crr;
+		crr = nxt;
+	}
+
+	*h_r = prv;
 }
 
 /**
@@ -35,31 +35,31 @@ void reverse(listint_t **h_r)
  */
 int compare(listint_t *h1, listint_t *h2)
 {
-    listint_t *tmp1;
-    listint_t *tmp2;
-    
-    tmp1 = h1;
-    tmp2 = h2;
-    
-    while (tmp1 != NULL && tmp2 != NULL)
-    {
-	if (tmp1->n == tmp2->n)
-	{
-	    tmp1 = tmp1->next;
-	    tmp2 = tmp2->next;
-	}
-	else
-	{
-	    return (0);
-	}
-    }
+	listint_t *tmp1;
+	listint_t *tmp2;
 
-    if (tmp1 == NULL && tmp2 == NULL)
-    {
-	return (1);
-    }
+	tmp1 = h1;
+	tmp2 = h2;
 
-    return (0);
+	while (tmp1 != NULL && tmp2 != NULL)
+	{
+		if (tmp1->n == tmp2->n)
+		{
+			tmp1 = tmp1->next;
+			tmp2 = tmp2->next;
+		}
+		else
+		{
+	return (0);
+		}
+	}
+
+	if (tmp1 == NULL && tmp2 == NULL)
+	{
+		return (1);
+	}
+
+	return (0);
 }
 
 /**
@@ -67,48 +67,48 @@ int compare(listint_t *h1, listint_t *h2)
  * is a palindrome
  * @head: pointer to head of list
  * Return: 0 if it is not a palindrome,
- * 1 if it is a palndrome
+ * 1 if it is a palindrome
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *slow, *fast, *prev_slow;
-    listint_t *scn_half, *middle;
-    int isp;
-    
-    slow = fast = prev_slow = *head;
-    middle = NULL;
-    isp = 1;
-    
-    if (*head != NULL && (*head)->next != NULL)
-    {
-	while (fast != NULL && fast->next != NULL)
+	listint_t *slow, *fast, prev_slow;
+	listint_t *scn_half, *middle;
+	int isp;
+
+	slow = fast = prev_slow = *head;
+	middle +NULL;
+	isp = 1;
+
+	if (*head != NULL && (*head)->next != NULL)
 	{
-	    fast = fast->next->next;
-	    prev_slow = slow;
-	    slow = slow->next;
+		while (fast != NULL && fast->next != NULL)
+		{
+			fast = fast->next->next;
+			prev_slow = slow;
+			slow = slow->next;
+		}
+
+		if (fast != NULL)
+		{
+			middle = slow;
+			slow = slow->next;
+		}
+
+		scn_half = slow;
+		prev_slow->next = NULL;
+		reverse(&scn_half);
+		isp = compare(*head, scn_half);
+
+		if (middle != NULL)
+		{
+			prev_slow->next = middle;
+			middle->next = scn_half;
+		}
+		else
+		{
+			prev_slow->next = scn_half;
+		}
 	}
-	
-	if (fast != NULL)
-	{
-	    middle = slow;
-	    slow = slow->next;
-	}
-	
-	scn_half = slow;
-	prev_slow->next = NULL;
-	reverse(&scn_half);
-	isp = compare(*head, scn_half);
-	
-	if (middle != NULL)
-	{
-	    prev_slow->next = middle;
-	    middle->next = scn_half;
-	}
-	else
-	{
-	    prev_slow->next = scn_half;
-	}
-    }
-    
-    return (isp);
+
+	return (isp);
 }
