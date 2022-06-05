51d93d60ab3df9ca1f291e324ec86c44d636ac85
Zerihunabebe <abebezerihunalx.com>
2022-06-05 01:37:45 -0700

    4_new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    """Replace an element at specific ndex without modifying the whole list"""
-    if idx < o or idx > (len(my_list) - 1):
+    """Replace an element at specific index without modifying the whole list"""
+    if idx < 0 or idx > (len(my_list) - 1):
         return my_list
 
     copy = [x for x in my_list]
