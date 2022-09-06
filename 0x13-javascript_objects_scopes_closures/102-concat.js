90227677cf3a05bd792cc1e6650aa044a6d762e8
Zerihunabebe <abebezerihunalx.com>
2022-09-06 11:19:47 -0700

    advanced task

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 
 const contentA = fs.readFileSync(process.argv[2], 'utf8', function (err, result) { if (err) console.log('error', err); });
-const contentB = fs.readFileSync(process.argv[3], 'utf8' function (err, result) { if (err) console.log('error', err); });
+const contentB = fs.readFileSync(process.argv[3], 'utf8', function (err, result) { if (err) console.log('error', err); });
 
 const contentC = contentA.concat(contentB);
 
