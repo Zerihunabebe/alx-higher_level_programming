7fd8073493da451e1c041f522c005c9309e92620
Zerihunabebe <abebezerihunalx.com>
2022-10-18 07:06:56 -0700

    updated

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@ const request = require('request');
 const { argv } = require('process');
 
 const BaseUrl = 'https://swapi-api.hbtn.io/api';
-requestBaseUrl + '/films/' + argv[2], (error, response, body) => {
+request(BaseUrl + '/films/' + argv[2], (error, response, body) => {
   if (error) {
     console.error(error);
   }
